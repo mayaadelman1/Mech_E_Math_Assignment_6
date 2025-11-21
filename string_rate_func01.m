7f42c808d1615f01196cb98a9f3b4ff8177e713a
mayaadelman1 <madelman@olin.edu>
2025-11-21 15:59:50 -0500

    string_rate_func01 possibly done

--- string_rate_func01.m
+++ string_rate_func01.m
@@ -13,27 +13,39 @@
 % string_params.c: %damping coefficient
 % string_params.dx: %horizontal spacing between masses
 function dVdt = string_rate_func01(t,V,string_params)
-n = string_params.n; %number of masses
-M = string_params.M; %total mass attached to the string
-Uf_func = string_params.Uf_func; %function describing motion of end point
-dUfdt_func = string_params.dUfdt_func; %time derivative of Uf
-Tf = string_params.Tf; %tension in string
-L = string_params.L; %length of string
-c = string_params.c; %damping coefficient
-dx = string_params.dx; %horizontal spacing between masses
-%unpack state variable
-U = V(1:n);
-dUdt = V((n+1):(2*n));
-Uf = Uf_func(t);
-dUfdt = dUfdt_func(t);
-%compute acceleration
-d2Udt2 = -inv(M)*U
-K = zeros(n);
+    n = string_params.n; %number of masses
+    M = string_params.M; %total mass attached to the string
+    Uf_func = string_params.Uf_func; %function describing motion of end point
+    dUfdt_func = string_params.dUfdt_func; %time derivative of Uf
+    Tf = string_params.Tf; %tension in string
+    L = string_params.L; %length of string
+    c = string_params.c; %damping coefficient
+    dx = string_params.dx; %horizontal spacing between masses
+    
+    %unpack state variable
+    U = V(1:n);
+    dUdt = V((n+1):(2*n));
 
-for i = 1:n
-    k(n, n) = -2;
-    if i > 1 || i < n
-end
-%assemble state derivative
-dVdt = [dUdt;d2Udt2];
+    Uf = Uf_func(t);
+    dUfdt = dUfdt_func(t);
+    
+    %compute acceleration
+    H = M/n*eye(n);
+    I = eye(n);
+    Q = -2*I + circshift(I, [0, 1]) + circhsift(I, [0, -1]);
+    Q(1, end) = 0;
+    Q(end, 1) = 0;
+    K = -Tf/dx * Q;
+    B = Tf/dx;
+    d2Udt2 = (-K * U + B*Uf)/H;
+
+    % d2Udt2 = -inv(M)*U
+    % K = zeros(n);
+    % 
+    % for i = 1:n
+    %     k(n, n) = -2;
+    %     if i > 1 || i < n
+    % end
+    % %assemble state derivative
+    dVdt = [dUdt;d2Udt2];
 end
