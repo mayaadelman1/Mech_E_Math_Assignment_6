22f8e04f9b2bacd45ad4b438c5e42aee75d087ce
mayaadelman1 <madelman@olin.edu>
2025-12-02 16:58:07 -0500

    modal analysis progress 1

--- laplacian_matrix.m
+++ laplacian_matrix.m
@@ -1,17 +1,11 @@
 function laplacian_matrix
-    H = (M/n)*eye(n);
-    I_n = eye(n);
-    Q = -2*I + circshift(I, [0, 1]) + circshift(I, [0, -1]);
-    Q(1, end) = 0;
-    Q(end, 1) = 0;
-    K = -Tf/dx * Q;
-    B = Tf/dx;
     %construct the nxn discrete laplacian matrix
-    n = % your code here
+    n = 3;
+    H = (M/n)*eye(n);
     I_n = eye(n); % build the nxn identity matrix
-    my_Laplacian = %your code here
-    %doing this as subtracting deals with the weird edge cases
-    %that occur when n=1 and n=2
+    my_Laplacian = -2*I_n + circshift(I_n, [0, 1]) + circshift(I_n, [0, -1]);
     my_Laplacian(1,end) = my_Laplacian(1,end)-1; %delete unwanted 1 in top right corner
     my_Laplacian(end,1) = my_Laplacian(end,1)-1; %delete unwanted 1 in bottom right corner
+    K = -Tf/dx * Q;
+    B = Tf/dx;
 end
